b2b8f2e602ece3f70fc6411c9cf5e7b6829633c1
root <root@DESKTOP-M8MN5F0.localdomain>
2018-05-05 04:39:40 +0900

    initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'denx-launcher'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
